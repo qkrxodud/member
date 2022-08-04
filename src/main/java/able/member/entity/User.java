32fc6d3f2e82e4c8aaa027c44ed4b42b878d3ff3
qkrxodud00 <qkrxodud00@gmail.com>
2022-08-04 19:02:54 +0900

    *회원가입 유효성 체크

--- src/main/java/able/member/entity/User.java
+++ src/main/java/able/member/entity/User.java
@@ -10,6 +10,8 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -27,17 +29,27 @@ public class User implements UserDetails {
     @Column(name = "user_no")
     private Long userNo;
 
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @Column(nullable = false, length = 100)
+    @Column(name = "password", nullable = false, length = 100)
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private String password;
 
     @Column(name = "mail", nullable = false, unique = true, length = 30)
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
     private String mail;
+
     @Column(name = "nick_name", nullable = false, length = 30)
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
+    @NotBlank(message = "닉네임은 필수 입력 값입니다.")
     private String nickName;
+
     @Column(name = "name", nullable = false, length = 30)
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "이름은 특수문자를 제외한 2~10자리여야 합니다.")
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
+
     @Column(name = "phone_number", nullable = false, length = 30)
+    @NotBlank(message = "핸드폰 번호는 필수 입력 값입니다.")
     private String phoneNumber;
 
     @ElementCollection(fetch = FetchType.EAGER)
